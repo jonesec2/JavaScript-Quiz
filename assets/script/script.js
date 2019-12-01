23d6c8eba8ad39121a6198aa00ef2d0449193643
pb-totoro <jonesec2@gmail.com>
2019-11-30 21:40:20 -0500

    Created html pages

---
+++ assets/script/script.js
@@ -0,0 +1,8 @@
+// var startButton = document.getElementById("start")
+
+// function startButton() {
+//     var startButton = document.getElementById("start").onclick = 
+
+    
+//     console.log(elem)
+// }
