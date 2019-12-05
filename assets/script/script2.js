ca61ecca1aeb4b3abed6be2d63cc6857cdd6a4e0
pb-totoro <jonesec2@gmail.com>
2019-12-05 15:18:15 -0500

    Added timer and local storage

--- assets/script/script2.js
+++ assets/script/script2.js
@@ -1,73 +1,105 @@
+quizTimer();
+
 function Quiz(questions) {
     this.score = 0;
     this.questions = questions;
     this.questionIndex = 0;
 }
- 
-Quiz.prototype.getQuestionIndex = function() {
+
+Quiz.prototype.getQuestionIndex = function () {
     return this.questions[this.questionIndex];
 }
- 
-Quiz.prototype.guess = function(answer) {
-    if(this.getQuestionIndex().isCorrectAnswer(answer)) {
+
+// quiz timer
+var timerEl = document.getElementById("timer");
+function quizTimer() {
+    var timeLeft = 75;
+
+    var timeInterval = setInterval(function () {
+        timerEl.textContent = "Time remaining: " + timeLeft + " second(s)";
+        timeLeft--
+
+        if (timeLeft <= -1) {
+            timeLeft = -1
+            clearInterval(timeInterval);
+            showScores();
+        }
+        else if (getQuestionIndex().isCorrectAnswer(!answer)) {
+            timeLeft = timeLeft - 5;
+        }
+    }, 1000);
+};
+
+Quiz.prototype.guess = function (answer) {
+
+    if (this.getQuestionIndex().isCorrectAnswer(answer)) {
         this.score++;
+        var rightAnswer = document.getElementById("rightWrong")
+        rightAnswer.innerHTML = "<hr> Right! <hr>"
     }
- 
+    else {
+        var wrongAnswer = document.getElementById("rightWrong")
+        wrongAnswer.innerHTML = "<hr> Wrong! <hr>"
+    }
     this.questionIndex++;
 }
- 
-Quiz.prototype.isEnded = function() {
+
+Quiz.prototype.isEnded = function () {
     return this.questionIndex === this.questions.length;
 }
- 
- 
+
+
 function Question(text, choices, answer) {
     this.text = text;
     this.choices = choices;
     this.answer = answer;
 }
- 
-Question.prototype.isCorrectAnswer = function(choice) {
+
+
+Question.prototype.isCorrectAnswer = function (choice) {
     return this.answer === choice;
 }
- 
- 
+
+
 function populate() {
-    if(quiz.isEnded()) {
+    if (quizVar.isEnded()) {
         showScores();
 
     }
     else {
         // show question
         var element = document.getElementById("question");
-        element.innerHTML = quiz.getQuestionIndex().text;
- 
+        element.innerHTML = quizVar.getQuestionIndex().text;
+
         // show options
-        var choices = quiz.getQuestionIndex().choices;
-        for(var i = 0; i < choices.length; i++) {
+        var choices = quizVar.getQuestionIndex().choices;
+        for (var i = 0; i < choices.length; i++) {
             var element = document.getElementById("choice" + i);
             element.innerHTML = choices[i];
             guess("btn" + i, choices[i]);
         }
- 
-        showProgress();
     }
 };
- 
+
 function guess(id, guess) {
     var button = document.getElementById(id);
-    button.onclick = function() {
-        quiz.guess(guess);
+    button.onclick = function () {
+        quizVar.guess(guess);
         populate();
     }
 };
 
- 
- 
+var submitButton = document.getElementById("submitHighScore")
+var highScoreInput = document.querySelector("#scoreName")
+var msgDiv = document.querySelector("#msg");
+
+
 function showScores() {
-    var gameOverHTML = "<h1 class='gameOver'>Result</h1>";
-    gameOverHTML += "<h2 id='score'> Your scores: " + quiz.score + "</h2>";
-    gameOverHTML += "<h2> <a style='color: white' class='nav-link' href='highScore.html'>High Scores</a> </h2>"
+    var gameOverHTML = "<hr><h1 class='gameOver'>Game Over!</h1>";
+    gameOverHTML += "<h2 id='score'> Your score: " + quizVar.score + "</h2>";
+    gameOverHTML += "<h4 id='enterScore'> Enter your name to the High Scores list</h4>";
+    gameOverHTML += "<form method='POST'><label for='highScore'>Name: </label> <input type='text' name='highScore' id='scoreName' placeholder='Spongebob'/> <button id='submitHighScore'>Submit</button></form> ";
+    gameOverHTML += "<div id='msg'></div> <hr>"
     var element = document.getElementById("quiz");
     element.innerHTML = gameOverHTML;
     document.getElementById("btn0").setAttribute("style", "opacity: 0; pointer-events: none;");
@@ -75,27 +107,79 @@ function showScores() {
     document.getElementById("btn2").setAttribute("style", "opacity: 0; pointer-events: none;");
     document.getElementById("btn3").setAttribute("style", "opacity: 0; pointer-events: none;");
     document.getElementById("question").setAttribute("style", "opacity: 0; pointer-events: none;");
-    push
 
+    var submitButton = document.getElementById("submitHighScore")
+    var highScoreInput = document.querySelector("#scoreName")
+    var msgDiv = document.querySelector("#msg");
+
+
+    function displayMessage(type, message) {
+        msgDiv.textContent = message;
+        msgDiv.setAttribute("class", type);
+    }
+
+
+    submitButton.addEventListener("click", function (event) {
+        event.preventDefault();
+
+        var scoreName = highScoreInput.value.trim();
+        var goToScore = "<h1><a id='start' href='highScore.html'> See High Scores <span </a></h1>"
+
+        if (scoreName === "") {
+            displayMessage("error", "Please enter your name :)");
+        } else {
+            displayMessage("success", element.innerHTML = goToScore)
+            localStorage.setItem("Name", scoreName);
+            localStorage.setItem("HighScore", quizVar.score)
+        }
+    })
 };
 
-// store high scores in local storage
+// attempt at dynamic high scores
+// var createHighScores = function (name, score) {
+//     var userEntry = JSON.parse(localStorage.getItem('names')) || [];
+
+//     var newHighScore = {
+
+//     };
+
+//     userEntry.push(newHighScore);
+
+//     localStorage.setItem('names', JSON.stringify(userEntry));
+// };
+
+
+// use local storage
+var usersName = document.querySelector("#usersName");
+var usersScore = document.querySelector("#usersScore");
+
+var localName = localStorage.getItem("Name");
+var localScore = localStorage.getItem("HighScore");
+if (usersName !== null) {
+    usersName.innerHTML = localName;
+} else {
+};
+if (usersScore !== null) {
+    usersScore.textContent = localScore;
+} else {
+};
 
 // create timer function
 
+
 //create timer score
- 
+
 // create questions here
 var questions = [
-    new Question("Question 1: What does DOM stand for?", ["1. sdfg", "2. sdfg","3. dfg", "4. fg"], "1. sdfg"),
-    new Question("Question 2: You Like Jazz?", ["1. sdfg", "2. sdfg","3. dfg", "4. fg"], "1. sdfg"),
-    new Question("Question 3: Who is George Washington?", ["1. sdfg", "2. sdfg","3. dfg", "4. fg"], "1. sdfg"),
-    new Question("Question 4: Can a woodchuck chuck wood?", ["1. sdfg", "2. sdfg","3. dfg", "4. fg"], "1. sdfg"),
-    new Question("Question 5: Zzzsf sdddf sss", ["1. sdfg", "2. sdfg","3. dfg", "4. fg"], "1. sdfg")
+    new Question("Question 1: What does DOM stand for?", ["1. sdfg", "2. sdfg", "3. dfg", "4. fg"], "1. sdfg"),
+    new Question("Question 2: You Like Jazz?", ["1. sdfg", "2. sdfg", "3. dfg", "4. fg"], "1. sdfg"),
+    new Question("Question 3: Who is George Washington?", ["1. sdfg", "2. sdfg", "3. dfg", "4. fg"], "1. sdfg"),
+    new Question("Question 4: Can a woodchuck chuck wood?", ["1. sdfg", "2. sdfg", "3. dfg", "4. fg"], "1. sdfg"),
+    new Question("Question 5: Zzzsf sdddf sss", ["1. sdfg", "2. sdfg", "3. dfg", "4. fg"], "1. sdfg")
 ];
- 
+
 // create quiz
-var quiz = new Quiz(questions);
- 
+var quizVar = new Quiz(questions);
+
 // display quiz
 populate();
